35497c4e81f73904b018b3d72f9c7c6a73b54fcc
amirreza ahmadi <ahmadiamirreza81@gmail.com>
2024-10-10 16:48:18 +0330

    Update root.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import { Landing } from "@/components/landing";
-import PoseDetection from "@/components/pos detector";
-import Game from "@/components/game1";
+
 export default function Page() {
   return (
     <div>
       <Landing />
-      {/* <PoseDetection/> */}
-      {/* <Game id={"1"}/> */}
+
     </div>
   )
 }
